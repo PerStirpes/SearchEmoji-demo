a00bb2d8ddb729d1354fddb86949c7ae2da555ba
Matt Carl <mccarl@asu.edu>
2017-03-23 15:09:17 -0700

    dynamic-layout: add props
    Learned: if text is added in btwn the <div></div> it will be displayed
    in the browser

--- src/EmojiResultRow.js
+++ src/EmojiResultRow.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 class EmojiResultRow extends React.Component {
   render() {
+    const codePointHex = this.props.symbol.codePointAt(0).toString(16);
+    const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
     return (
       <div>
-        Welcome to EmojiResultRow
+        <img alt={this.props.title} src={src} />
       </div>
     );
   }
 }
+EmojiResultRow.propTypes = {
+  title: React.PropTypes.string,
+  symbol: React.PropTypes.string
+};
 export default EmojiResultRow;
